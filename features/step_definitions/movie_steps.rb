1241a702f0697d94558b6d10a8af431d169a7d86
pepemontana7 <jose.a.delgado.lopez@gmail.com>
2013-04-28 23:25:28 -0700

    finsih scenrio1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -10,3 +10,9 @@ Given /the following movies exist/ do |movies_table|
 
   assert movies_table.hashes.size == Movie.all.count
 end
+
+Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |arg1, arg2|
+   # express the regexp above with the code you wish you had
+   movie = Movie.find_by_title(arg1)
+   assert movie.director == arg2
+end
